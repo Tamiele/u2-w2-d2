bc6f51fb16dc53d0f281fcc715d2766bf3e4e40a
Tamiele <tammaro_85@hotmail.it>
2024-09-17 19:59:47 +0200

    primo extra con js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+//extra 16
+const addBadge = function () {
+  let cards = document.querySelectorAll("#welcomeSummer .card");
+  for (i = 0; i < cards.length; i++) {
+    let card = cards[i];
+    let span = document.createElement("span");
+    span.className = "badge text-bg-danger position-absolute end-0 mt-2 me-2";
+    span.innerText = "HOT";
+    card.appendChild(span);
+  }
+};
 
+addBadge();
